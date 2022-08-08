8731161cc685025e4376183309b039e992cd1497
gan-of-culture <project.ganoc@gmail.com>
2022-08-08 21:24:46 +0200

    [REMOVED] koushoku.org, hentai.guru and hentaistream.io for having broken websites for multiple weeks and dropping support for their websites

--- extractors/jwplayer/jwplayer_test.go
+++ extractors/jwplayer/jwplayer_test.go
@@ -12,9 +12,9 @@ func TestExtract(t *testing.T) {
 		Args test.Args
 	}{
 		{
-			Name: "Single Episode hentai.guru",
+			Name: "Single Episode hentaihaven.xxx",
 			Args: test.Args{
-				URL:     "https://hentai.guru/wp-content/plugins/player-logic/player.php?data=bDFCblBOMTZvcVAwZ2E2M1VMeUpldUc2MzNpeGUwZEc5dGF5RlRVWlBnbjJXQWV2YzN4RmJwemJjVno1TUYvWXRvMnNaNjhPMEdDdDJ1RlA0ZFVZYyt1WksybnFsd2lxZW1pQzJMSzYzd05Vdk1FVzEyeStaS3c1ekpqSnFaNmlPZE96UUM2VzljdlRJb0Zkc0tqVlJnPT06fDo6fDpzN0t3UmFNcnRNYUdJa3FrbENKSzhnPT0=",
+				URL:     "https://hentaihaven.xxx/wp-content/plugins/player-logic/player.php?data=OTZPUXF5OU1DbWxVSzROajdkWkx6T1Y4RFBVZ3JQKzRqWlB3M1FXY2tudUU2S1VHNklsUHY0TmRWREtCd3pKb2Q3ZVVtNklCWDJRUndqNGZQWjR5MjJKOEo2ZXU3QzBmTEU5aTRKK2tGME1MazZZUzh4Y0ZMRThyOG9TOTlHMnM0TnZvQnRIelhHTTJrL1htYUVjUEFRPT06fDo6fDp2bFlyazVWNmdCaU1BMTVOM0U4SGNBPT0=",
 				Title:   "jwplayer video",
 				Quality: "1920x1080",
 				Size:    475164360,
