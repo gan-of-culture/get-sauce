8dc092c0e6a3584a0bd9e33303f4a41b00e01f7d
gan-of-culture <project.ganoc@gmail.com>
2023-12-17 03:08:08 +0100

    [extractor:universal] update tests

--- extractors/universal/universal_test.go
+++ extractors/universal/universal_test.go
@@ -23,10 +23,10 @@ func TestExtract(t *testing.T) {
 		{
 			Name: "with bloat after ext",
 			Args: test.Args{
-				URL:     "https://img.rule34.xxx//images/1979/b84be533024a3d1dcc6b01c0cb7358c9.jpeg?2686173",
-				Title:   "b84be533024a3d1dcc6b01c0cb7358c9",
+				URL:     "https://wimg.rule34.xxx//images/41/d54d1f1dde83ff670c5932ab8f2d42a5cf18e587.jpg?40602",
+				Title:   "d54d1f1dde83ff670c5932ab8f2d42a5cf18e587",
 				Quality: "",
-				Size:    143583,
+				Size:    125079,
 			},
 		},
 	}
